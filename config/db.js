1ed77b170a78b38c064dbbf5be55da0d32c72c02
Unknown <romain.gillier@hotmail.fr>
2018-05-25 16:12:07 +0200

    idem

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : brb6vf1az-mysql.services.clever-cloud.com,
-  user     : u6uxk7x23cng8vtz,
-  password : aHgHKDiwop0ajtWxr7p,
-  database : brb6vf1az,
+  host     : 'brb6vf1az-mysql.services.clever-cloud.com',
+  user     : 'u6uxk7x23cng8vtz',
+  password : 'aHgHKDiwop0ajtWxr7p',
+  database : 'brb6vf1az',
   dateStrings: 'date',
   multipleStatements: true
 });
